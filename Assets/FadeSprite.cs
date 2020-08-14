f5ed61c9b93554b41d39e404c0fe62439a7385d3
Huynh Nguyen Bac Giang <1851050037giang@ou.edu.vn>
2020-08-15 00:04:57 +0700

    giang
    tao moi truong unity

---
+++ Assets/FadeSprite.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeSprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    SpriteRenderer Rend;
+    void Start()
+    {
+        Rend = GetComponent<SpriteRenderer>();
+    }
+
+    IEnumerator FadeOut()
+    {
+        for(float f = 1f; f >= 0.5f; f -= 0.05f)
+        {
+            Color c = Rend.material.color;
+            c.a = f;
+            Rend.material.color = c;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    IEnumerator FadeIn()
+    {
+        for (float f = 0.5f; f <= 1f; f += 0.05f)
+        {
+            Color c = Rend.material.color;
+            c.a = f;
+            Rend.material.color = c;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        StartCoroutine("FadeOut");       
+    }
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        StartCoroutine("FadeIn");
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 1f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 movement;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
