c0d08a47a25625bef04146e00a4940be514a99a2
Huynh Nguyen Bac Giang <1851050037giang@ou.edu.vn>
2020-08-21 00:34:02 +0700

    edit player moverment

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -5,28 +5,59 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float moveSpeed = 1f;
-
-    public Rigidbody2D rb;
-
+    public float moveSpeed = 7f;
+    private Rigidbody2D rb;
     public Animator animator;
+    private Vector2 change;
 
-    Vector2 movement;
-
+    void Start() {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        change = Vector2.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine (AttackAnimation());
+        }
+        else
+            UpdateAnimationAndMove();
     }
 
+    private IEnumerator AttackAnimation()
+    {
+        animator.SetBool("attacking", true);
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3f);
+
+    }
+
+    void UpdateAnimationAndMove() {
+        if (change != Vector2.zero)
+        {
+            MovePlayer();
+            animator.SetFloat("Horizontal", change.x);
+            animator.SetFloat("Vertical", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MovePlayer()
+    {
+        rb.MovePosition(rb.position + change * moveSpeed * Time.deltaTime);
+    }
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
     }
 }
