6c3b896de1ee02ddbc456c9d78ce30a04476107c
Huynh Nguyen Bac Giang <1851050037giang@ou.edu.vn>
2020-09-09 17:53:04 +0700

    Menu update

---
+++ Assets/DeathMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GameIsEnd = false;
+    public GameObject deathMenuUI;
+    public GameObject deathMenuBackGr;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameObject.Find("Player").GetComponent<Player>().status)
+        {
+            death();
+        }
+    }
+    private void death() {
+        deathMenuUI.SetActive(true);
+        deathMenuBackGr.SetActive(true);
+    }
+
+}
